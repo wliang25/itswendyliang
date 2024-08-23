2475e9f52606ec3e8f206f9ed71005fcf5107d74
Wendy Liang <wendyl.y.123@gmail.com>
2024-08-23 04:29:18 -0700

    add projects and update page

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import './App.css';
 import WendyLiang from './WendyLiang.js';
+import Projects from './Projects.js';
+import Resume from './Resume.js';
 
 const App = () => {
   const [activeTab, setActiveTab] = useState('wendyliang');
@@ -9,12 +11,15 @@ const App = () => {
     <div className='app'>
       <div className='tabs'>
         <div className={`tab ${activeTab === 'wendyliang' && 'active'}`} onClick={() => { setActiveTab('wendyliang')}}>wendyliang</div>
+        <div className={`tab ${activeTab === 'projects' && 'active'}`} onClick={() => { setActiveTab('projects') }}>projects</div>
         <div className={`tab ${activeTab === 'resume' && 'active'}`} onClick={() => { setActiveTab('resume') }}>resume</div>
       </div>
-      <div className='tabsection'>
-        {activeTab === 'wendyliang' && <div className='tabcontent'><WendyLiang /></div>}
-        {activeTab === 'resume' && <div className='tabcontent'>resume content</div>}
+      <div className='tabcontent'>
+        {activeTab === 'wendyliang' && <WendyLiang />}
+        {activeTab === 'projects' && <Projects />}       
+        {activeTab === 'resume' && <Resume />}
       </div>      
+      <footer className='footer'><p>&copy; 2024 Wendy Liang. All Rights Reserved.</p></footer>
     </div>
   );
 }
--- src/App/WendyLiang.js
+++ src/App/WendyLiang.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import pfp from './imgs/profile.jpg'
 
 const WendyLiang = () => {
   return (
-    <div>
-      <img src='profile.jpg' alt='profile' />
-      <h1>Hi, I'm Wendy!</h1>
-      <p>An alumni of the University of Washington that's excited to explore and experience the intersections of technology and everyday applications. 
-        I majored in Geography with a focus on Geographic Information Systems (GIS) and double minored in Informatics and Philosophy.</p>
+    <div className='wendyliang'>
+      <img src={pfp} alt='profile' />
+      <div className='pftext'>
+        <h1>Hi, I'm Wendy!</h1>
+        <p>An alumni of the University of Washington that's excited to explore and experience the intersections of technology and everyday applications. 
+          I majored in Geography with a focus on Geographic Information Systems (GIS) and double minored in Informatics and Philosophy.</p>  
+        <p>After graduating from the University of Washington, I interned at CBRE as a Software Engineer Intern where I was offered a full time position
+          after my internship. At CBRE I worked on developing reusable React and Web components for multiple internal design libraries to ensure consistent
+          UI/UX across applications. I also played a key role in the development of SiteIQ Survey Creator, building frontend microservices with a separate design
+          library, integrated third-party services like Google Analytics and Flatfile to streamline market data presentation. I have experiences working in an
+          agile development environment and collaboration with product and design teams.
+        </p>
+        <p>To further my skillsets, I am currently looking into getting my Masters in Science
+          from WGU with their Data Analytics - Data Engineering program.
+        </p>
+      </div>
     </div>
   );
 }
